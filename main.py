598ee4cc83ff1848fe6da71505b13256943951af
0xlvl3 <cryptochoccy@gmail.com>
2023-01-19 17:25:53 +1100

    Starting to create GUI functions and connect Firebase

--- main.py
+++ main.py
@@ -12,16 +12,39 @@ from modules.play_cards import play_cards
 from modules.inspect_deck import inspect_deck
 from modules.delete_deck import delete_deck
 from modules.open_menu import open_menu
+from firebase import signup
+from firebase import user_login
 
 Builder.load_file("frontend.kv")
 
 
 class StartScreen(Screen):
-    def switch_deck_viewer(self):
-        self.manager.current = "deck_viewer"
+    def create_account(self):
+        self.manager.current = "create_screen"
+
+    def go_to_login(self):
+        self.manager.current = "login_screen"
 
 
-class DeckViewer(Screen):
+class CreateScreen(Screen):
+    def create(self):
+        email = self.manager.current_screen.ids.user_email.text
+        password = self.manager.current_screen.ids.user_password.text
+        signup(email, password)
+        print("Account created")
+        self.manager.current = "login_screen"
+
+
+class LoginScreen(Screen):
+    def login(self):
+        print("Logging in")
+        email = self.manager.current_screen.ids.login_email.text
+        password = self.manager.current_screen.ids.login_password.text
+        user_login(email, password)
+        self.manager.current = "home_screen"
+
+
+class HomeScreen(Screen):
     def switch_cards_screen(self):
         self.manager.current = "cards_screen"
 
