cedc548b5f0330e3ebc59847937367078cc8522e
0xlvl3 <cryptochoccy@gmail.com>
2023-01-20 10:58:37 +1100

    Deck class functional

---
+++ deck.py
@@ -0,0 +1,61 @@
+from test_fb import db
+from datetime import datetime
+
+
+class Deck:
+    def __init__(self):
+        pass
+
+    def create_deck(self):
+        deck_name = input("Name your new deck: ")
+        db.child(deck_name).set("flashcards")
+
+    def add_flashcards(self):
+        user_deck = input("Which deck are we placing these in: ")
+        user_question = input("Question: ")
+        user_answer = input("Answer: ")
+
+        data = {user_question: user_answer}
+        current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        db.child(user_deck).child("flashcards").child(current_time).set(data)
+
+    def delete_deck(self, deck):
+        db.child(deck).remove()
+
+    def inspect_deck(self, deck):
+
+        try:
+            ins = db.child(deck).child("flashcards").get()
+            card_total = len(list(ins.val()))
+            index = 0
+            card_count = 0
+
+            while index != card_total:
+                start = list(ins.val().keys())[index]
+                card_count += 1
+                index += 1
+                flashcards = ins.val()[start]
+                for key, value in flashcards.items():
+                    print(f"{card_count}. Question: {key}")
+                    print(f"Answer: {value}\n")
+
+        except Exception as e:
+            e = "You've entered a deck that doesn't exist"
+            print(e)
+
+
+deck = Deck()
+
+# Create a deck.
+# deck.create_deck()
+
+# Add flashcards + create a deck.
+# deck.add_flashcards()
+
+# Remove a deck.
+# user_input = input("Remove what deck: ")
+# deck.delete_deck(user_input)
+
+# Inspect deck.
+user_input = input("What deck will you view: ")
+deck.inspect_deck(user_input)
