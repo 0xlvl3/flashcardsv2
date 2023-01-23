2ef88b8ec4bfd3165118110a88a3ff48e8558d69
0xlvl3 <cryptochoccy@gmail.com>
2023-01-23 22:19:24 +1100

    Testing

--- screens/createdeckscreen.py
+++ screens/createdeckscreen.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import Screen
 from deck import user_deck
+from tkm import TokenManager
 
 
 class CreateDeckScreen(Screen):
@@ -7,8 +8,9 @@ class CreateDeckScreen(Screen):
         """
         Function will take user input and create a deck.
         """
+        token = TokenManager.get_token()
         deck = self.manager.current_screen.ids.deck_to_create.text
-        user_deck.create_deck(deck)
+        user_deck.create_deck(user=token, deck=deck)
         self.manager.current_screen.ids.create_success.text = f"Your new deck {deck} is created! go back to the home screen and add flashcards!"
 
     def return_home(self):
